871936604b388613f99e96cb4ac7313804f5cd41
deveshcse <d.mishra56603@gmail.com>
2024-11-03 15:00:23 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen text-3xl font-bold underline flex justify-center items-center">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
