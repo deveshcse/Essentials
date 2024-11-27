425a24d0a0ae44635015b77fc9ba2253c6bc41ad
deveshcse <d.mishra56603@gmail.com>
2024-11-27 20:54:47 +0530

    completed filter component.

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Filters = () => {
   const [allCategories, setAllCategories] = useState();
@@ -14,23 +15,27 @@ const Filters = () => {
 
   useEffect(() => {
     getCategoriesList();
-    console.log(allCategories);
   }, []);
   return (
-    <div className="hidden  h-full md:h-[400px] col-span-1 text-gray-800 font-title text-base md:flex flex-col overflow-scroll "   style={{
-      scrollbarWidth: "none", // Hides scrollbar in Firefox
-      msOverflowStyle: "none", // Hides scrollbar in Internet Explorer/Edge
-    }}>
+    <div
+      className="hidden  h-full md:h-[400px] col-span-1 text-gray-800 font-title text-base md:flex flex-col overflow-scroll "
+      style={{
+        scrollbarWidth: "none", // Hides scrollbar in Firefox
+        msOverflowStyle: "none", // Hides scrollbar in Internet Explorer/Edge
+      }}
+    >
       {allCategories &&
         allCategories.map((category) => (
-          <div
-            key={category}
-            className="flex items-center justify-between pb-4 capitalize"
-          >
-            <span>{category}</span>
-            <span className="mr-10">
-              <RiArrowRightSLine size={24} className="" />
-            </span>
+          <div key={category} className="pb-4">
+            <Link
+              to={/allProduct/ + category}
+              className="flex items-center justify-between  capitalize"
+            >
+              <span>{category}</span>
+              <span className="mr-10">
+                <RiArrowRightSLine size={24} className="" />
+              </span>
+            </Link>
           </div>
         ))}
     </div>
