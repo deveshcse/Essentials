35508d01505eafc6a1cbca93e47248ad56ae71f2
deveshcse <d.mishra56603@gmail.com>
2024-11-27 20:58:34 +0530

    added wishlist button functionality in product card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import RatingComponent from "./RatingComponent";
+import { useDispatch } from "react-redux";
+import { toggleWishlistItem } from "../Utils/WishlistSlice";
+import { useSelector } from "react-redux";
 
-const ProductCard = ({props, next, previous}) => {
+const ProductCard = ({ props }) => {
 
-  console.log(props);
-
-  const {title,thumbnail,price,rating,discountPercentage,reviews} = props;
-  console.log(title,thumbnail,price,rating,discountPercentage);
+  const { id, title, thumbnail, price, rating, discountPercentage, reviews } =
+    props;
   
+  const dispatch = useDispatch();
+  const wishlistItems = useSelector(state => state.wishlist.wishlistItems);
   
+  const isInWishlist =(id) =>{
+    return wishlistItems.some(item => item.id === id);
+  };
 
+  const handleClickOnWishlistButton = (e) => {
+    e.stopPropagation(); // Prevent the click from propagating to the Link
+    e.preventDefault();  // Prevent navigation if needed
+    dispatch(toggleWishlistItem(props));
+    
+  };
 
   return (
     <div className="min-w-64">
@@ -26,8 +36,20 @@ const ProductCard = ({props, next, previous}) => {
         <div className="absolute top-2 left-2 bg-red-500 px-2 py-1 rounded-md text-white text-sm font-title">
           {Math.round(discountPercentage)}%
         </div>
-        <div className="absolute top-2 right-2 bg-white p-1 rounded-full">
-          <FaRegHeart size={20} className="hover:text-gray-600 m-1" />
+        <div className="absolute top-2 right-2 bg-white p-1 rounded-full pointer-events-none">
+          {isInWishlist(id) ? (
+            <FaHeart
+              size={20}
+              className="m-1 pointer-events-auto text-red-500"
+              onClick={handleClickOnWishlistButton}
+            />
+          ) : (
+            <FaRegHeart
+              size={20}
+              className="m-1 pointer-events-auto hover:text-gray-600"
+              onClick={handleClickOnWishlistButton}
+            />
+          )}
         </div>
       </div>
 
@@ -37,10 +59,14 @@ const ProductCard = ({props, next, previous}) => {
         </div>
         <div className="flex gap-4">
           <span className="text-red-600">${price}</span>
-          <span className="text-gray-500 line-through">${Math.round(price / (1 - discountPercentage / 100))}</span>
+          <span className="text-gray-500 line-through">
+            ${Math.round(price / (1 - discountPercentage / 100))}
+          </span>
         </div>
         <div className="flex gap-4">
-          <div><RatingComponent rating={rating}/></div>
+          <div>
+            <RatingComponent rating={rating} />
+          </div>
           <span className="text-gray-500 font-semibold">{reviews.length}</span>
         </div>
       </div>
