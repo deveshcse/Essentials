a1dbeb861460bbfc3de02cb132bfe428b27e691d
deveshcse <d.mishra56603@gmail.com>
2024-11-30 22:55:57 +0530

    created and completed wishlist container

---
+++ src/Components/WishlistContainer.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import WishlistCard from "./WishlistCard";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Utils/CartSlice";
+import { removeFromWishlist } from "../Utils/WishlistSlice";
+const WishlistContainer = () => {
+  const wishlistItems = useSelector((state) => state.wishlist.wishlistItems);
+
+  const dispatch = useDispatch();
+  const handleDeleteButton = (id) => {
+    dispatch(removeFromWishlist(id));
+  };
+  const handleAddToCart = (data) => {
+    const product = {...data, orderQuantity: data.minimumOrderQuantity}
+    dispatch(addToCart(product));
+  };
+
+  return (
+    <div className="mt-10 md: mx-32">
+      <div className="text-xl font-title my-2"><span>Wishlist ({wishlistItems.length})</span></div>
+      <div className="flex flex-start gap-5 flex-wrap">
+        {wishlistItems &&
+          wishlistItems.map((item) => (
+            <WishlistCard
+              key={item.id}
+              data={item}
+              deleteButton={handleDeleteButton}
+              addToCart={handleAddToCart}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default WishlistContainer;
