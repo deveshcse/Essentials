1f6354cadb9e3fe296ef48ff5680b0278d2f1c49
deveshcse <d.mishra56603@gmail.com>
2024-11-30 22:48:27 +0530

    completed cart slice reducers definations

--- src/Utils/CartSlice.jsx
+++ src/Utils/CartSlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cartItems: [],
+  subtotal: 0,
 };
 
 const cartSlice = createSlice({
@@ -9,16 +10,45 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-        const item = action.payload;
+      const exists = state.cartItems.some(
+        (item) => item.id === action.payload.id
+      );
+      if (!exists) {
+        const item = {
+          ...action.payload,
+          itemSubtotal: Math.ceil(
+            action.payload.orderQuantity * action.payload.price
+          ),
+        };
         state.cartItems.push(item);
+      }
     },
     removeFromCart: (state, action) => {
-        state.cartItems= state.cartItems.filter((item) => item.id !== action.payload);
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
     },
-    updateQuantityInCart: (state, action) => {},
+    updateQuantity: (state, action) => {
+      const { value, id } = action.payload;
+
+      state.cartItems = state.cartItems.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              orderQuantity: value,
+              itemSubtotal: Math.ceil(value * item.price),
+            }
+          : item
+      );
+    },
+    calculateSubtotal: (state) => {
+      state.subtotal = state.cartItems.reduce((acc, item) => {
+        return acc + item.itemSubtotal;
+      }, 0);
+    },
   },
 });
 
-
-export const {addToCart, removeFromCart, updateQuantityInCart} = cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuantity, calculateSubtotal } =
+  cartSlice.actions;
 export default cartSlice.reducer;
